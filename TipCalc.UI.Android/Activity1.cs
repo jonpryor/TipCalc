68fbb8ed36db0f1a365aceddac69f9b4f144c62f
Jonathan Pryor <jonpryor@vt.edu>
2011-10-06 16:26:05 -0400

    [TipCalc.UI.Android] Implement the Android UI.

--- TipCalc.UI.Android/Activity1.cs
+++ TipCalc.UI.Android/Activity1.cs
@@ -7,12 +7,20 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using TipCalc.Util;
+
 namespace TipCalc.UI.Android
 {
 	[Activity (Label = "TipCalc.UI.Android", MainLauncher = true)]
 	public class Activity1 : Activity
 	{
-		int count = 1;
+		TipInfo info = new TipInfo () {
+			TipPercent = 15,
+		};
+
+		TextView TipPercent;
+		TextView Total;
+		TextView TipValue;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,12 +29,67 @@ namespace TipCalc.UI.Android
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++); };
+			TipValue    = FindViewById<TextView>(Resource.Id.TipValue);
+			Total       = FindViewById<TextView>(Resource.Id.Total);
+
+			TipPercent = FindViewById<TextView>(Resource.Id.TipPercent);
+			TipPercent.AfterTextChanged += (sender, e) => {
+				UpdateTipPercent (Parse (TipPercent));
+			};
+
+			FindViewById<SeekBar>(Resource.Id.TipPercentSeekbar).SetOnSeekBarChangeListener (new SeekBarChangeListener (this));
+
+			var subtotal = FindViewById<TextView>(Resource.Id.Subtotal);
+			subtotal.AfterTextChanged += (sender, e) => {
+				info.Subtotal = Parse (subtotal);
+				UpdateTipPercent (info.TipPercent);
+			};
+			var total = FindViewById<TextView>(Resource.Id.ReceiptTotal);
+			total.AfterTextChanged += (sender, e) => {
+				info.Total = Parse (total);
+				UpdateTipPercent (info.TipPercent);
+			};
+		}
+
+		static decimal Parse (TextView field)
+		{
+			try {
+				return Convert.ToDecimal (field.Text);
+			} catch (Exception e) {
+				field.Text = e.Message;
+				return 0m;
+			}
+		}
+
+		void UpdateTipPercent (decimal newPercent)
+		{
+			info.TipPercent = newPercent;
+			TipValue.Text = info.TipValue.ToString ();
+			Total.Text = (info.Total + info.TipValue).ToString ();
+		}
+
+		class SeekBarChangeListener : Java.Lang.Object, SeekBar.IOnSeekBarChangeListener {
+
+			Activity1 context;
+
+			public SeekBarChangeListener (Activity1 context)
+			{
+				this.context = context;
+			}
+
+			public void OnProgressChanged (SeekBar seekBar, int progress, bool fromUser)
+			{
+				context.TipPercent.Text = progress.ToString ();
+				context.UpdateTipPercent ((decimal) progress);
+			}
+
+			public void OnStartTrackingTouch (SeekBar seekBar)
+			{
+			}
+
+			public void OnStopTrackingTouch (SeekBar seekBar)
+			{
+			}
 		}
 	}
 }
--- TipCalc.UI.Android/Resources/Resource.designer.cs
+++ TipCalc.UI.Android/Resources/Resource.designer.cs
@@ -37,8 +37,23 @@ namespace TipCalc.UI.Android
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int ReceiptTotal = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int Subtotal = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int TipPercent = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int TipPercentSeekbar = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int TipValue = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int Total = 2131034117;
 			
 			private Id()
 			{
